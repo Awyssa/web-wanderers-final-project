69ef597878374f3ff749312c8f53daa5bca8a5b1
Alireza Bagheri <alirezabg@gmail.com>
2022-07-27 19:32:09 +0100

    bug fix

--- api/controllers/authController.ts
+++ api/controllers/authController.ts
@@ -2,11 +2,11 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import prisma from "../config/prisma";
 
-const registerUser = async(req, res) => {
+const registerUser = async(req: { body: { email: any; password: any; firstName: any; lastName: any; phoneNumber: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { message: unknown; }): any; new(): any; }; }; }) => {
 	const { email, password, firstName, lastName, phoneNumber } = req.body;
 
 	try {
-		const newUser = await prisma.User.create({
+		const newUser = await prisma.user.create({
 			data: {
 				email: email,
 				password: password,
@@ -18,14 +18,14 @@ const registerUser = async(req, res) => {
 		});
 
 		console.log("new user ", newUser.firstName);
-		return res.status(202).json({ message: `Welcome ${newUser.username}!` });
+		return res.status(202).json({ message: `Welcome ${newUser.email}!` });
 	} catch (err) {
 		console.log("error", err);
 		return res.status(422).json({ message: err });
 	}
 };
 
-const loginUser = async(req, res) => {
+const loginUser = async(req: { body: { userEmail: any; password: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; send: { (arg0: string): void; new(): any; }; json: { (arg0: { message: any; token?: string; }): any; new(): any; }; }; }) => {
 	const userEmail = req.body.userEmail;
 	const password = req.body.password;
 
@@ -49,8 +49,8 @@ const loginUser = async(req, res) => {
 		.status(200)
 		.json({ message: `Welcome back ${userToLogin.email}`, token });
 	} catch (err) {
-		console.log("AUTH ERROR!", err.message);
-		return res.status(422).json({ message: err.message });
+		// console.log("AUTH ERROR!", err.message);
+		// return res.status(422).json({ message: err.message });
 	}
 };
 
